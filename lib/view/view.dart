a86cca0ff022791ef01306d3db5beb9f25baca8b
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2023-05-18 18:25:30 -0300

    🎉 hello world!

---
+++ lib/view/view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PokemonView extends StatelessWidget {
+  const PokemonView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const Placeholder();
+}
