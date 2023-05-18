077ecf6036cebf255477bdf304e0ac030bba9f07
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2023-05-18 19:51:41 -0300

    🏗️ adjusted models

--- lib/core/model/details/abilities_details.dart
+++ lib/core/model/details/abilities_details.dart
@@ -1,5 +1,18 @@
 class AbilitiesDetails {
-  AbilitiesDetails();
+  AbilitiesDetails({
+    String? name,
+    String? url,
+  })  : _name = name,
+        _url = url;
 
-  factory AbilitiesDetails.fromJson(Map json) => AbilitiesDetails();
+  final String? _name;
+  final String? _url;
+
+  String get name => _name ?? "";
+  String get url => _url ?? "";
+
+  factory AbilitiesDetails.fromJson(Map json) => AbilitiesDetails(
+        name: json['name'],
+        url: json['url'],
+      );
 }
