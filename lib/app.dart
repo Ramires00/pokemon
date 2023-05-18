592f1842457bd832ae5b953654020bce94377ebc
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2023-05-18 20:30:16 -0300

    🏗️ adjusted structure for pagination

---
+++ lib/app.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+class App extends GetMaterialApp {
+  const App({super.key});
+
+  @override
+  Widget? get home => Container();
+}
