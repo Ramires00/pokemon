663d6540ff44377a0a58c2579da137d0d2940d65
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2023-05-18 19:51:55 -0300

    🏗️ adjusted models

--- lib/core/model/pokemon.dart
+++ lib/core/model/pokemon.dart
@@ -2,15 +2,15 @@ import 'package:pokemon/core/model/details/abilities_details.dart';
 import 'package:pokemon/core/model/details/sprites_details.dart';
 import 'package:pokemon/core/model/details/types_details.dart';
 
-class PokemonData {
-  PokemonData({
+class Pokemon {
+  Pokemon({
     int? id,
     String? name,
     int? height,
     int? weight,
-    List<AbilitiesDetails>? abilities,
-    List<SpritesDetails>? sprites,
-    List<TypesDetails>? types,
+    List? abilities,
+    SpritesDetails? sprites,
+    List? types,
   })  : _id = id,
         _name = name,
         _height = height,
@@ -23,36 +23,32 @@ class PokemonData {
   final String? _name;
   final int? _height;
   final int? _weight;
-  final List<AbilitiesDetails>? _abilities;
-  final List<SpritesDetails>? _sprites;
-  final List<TypesDetails>? _types;
+  final List? _abilities;
+  final SpritesDetails? _sprites;
+  final List? _types;
 
   int get id => _id ?? 0;
-  String? get name => _name ?? "";
+  String get name => _name ?? "";
   int get height => _height ?? 0;
   int get weight => _weight ?? 0;
-  List<AbilitiesDetails> get abilities => _abilities ?? [];
-  List<SpritesDetails> get sprites => _sprites ?? [];
-  List<TypesDetails> get types => _types ?? [];
+  List get abilities => _abilities ?? [];
+  SpritesDetails get sprites => _sprites ?? SpritesDetails();
+  List get types => _types ?? [];
 
-  factory PokemonData.fromJson(Map json) => PokemonData(
+  factory Pokemon.fromJson(Map json) => Pokemon(
         id: json['id'],
         name: json['name'],
         height: json['height'],
         weight: json['weight'],
         abilities: json['abilities']
             .map(
-              (ability) => AbilitiesDetails.fromJson(ability),
+              (ability) => AbilitiesDetails.fromJson(ability['ability']),
             )
             .toList(),
-        sprites: json['sprites']
-            .map(
-              (sprites) => SpritesDetails.fromJson(sprites),
-            )
-            .toList(),
+        sprites: SpritesDetails.fromJson(json['sprites']),
         types: json['types']
             .map(
-              (types) => TypesDetails.fromJson(types),
+              (types) => TypesDetails.fromJson(types['type']),
             )
             .toList(),
       );
