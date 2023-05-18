bbe342019954a28cd45f76436cab16b5731e8653
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2023-05-18 20:30:47 -0300

    🏗️ adjusted structure for pagination

--- lib/view/view.dart
+++ lib/view/view.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:pokemon/viewmodel/viewmodel_home.dart';
 
 class PokemonView extends StatelessWidget {
-  const PokemonView({Key? key}) : super(key: key);
+  PokemonView({Key? key}) : super(key: key);
+
+  final ViewModelHome _viewmodel = Get.find<ViewModelHome>();
 
   @override
-  Widget build(BuildContext context) => const Placeholder();
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+      );
+
+  Widget _list() => PagedListView(
+        pagingController: _viewmodel.pagingController,
+        builderDelegate: _builderDelegate(),
+      );
+
+  PagedChildBuilderDelegate _builderDelegate() => PagedChildBuilderDelegate(
+        itemBuilder: (context, item, index) {
+          return Placeholder();
+        },
+      );
 }
