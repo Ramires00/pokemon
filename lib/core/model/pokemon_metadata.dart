823cdd7c15fb99440dea1c12956ccc18061520ef
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2023-05-18 18:53:06 -0300

    🏗️ moved and organized models

---
+++ lib/core/model/pokemon_metadata.dart
@@ -0,0 +1,15 @@
+class PokemonMetadata {
+  PokemonMetadata({
+    String? name,
+    String? url,
+  }) : _name = name, _url = url;
+
+  final String? _name;
+  final String? _url;
+
+  String get name => _name ?? "";
+  String get url => _url ?? "";
+
+  factory PokemonMetadata.fromJson(Map json) => PokemonMetadata(url: json['url'], name: json['name']);
+
+}
