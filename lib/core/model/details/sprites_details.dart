9ef233214a4e3009b4042b90d8817c292adfdad3
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2023-05-18 19:51:44 -0300

    🏗️ adjusted models

--- lib/core/model/details/sprites_details.dart
+++ lib/core/model/details/sprites_details.dart
@@ -1,7 +1,18 @@
 class SpritesDetails {
-  final String? frontDefault;
-  SpritesDetails({this.frontDefault});
+  SpritesDetails({
+    String? frontDefault,
+    String? artwork,
+  })  : _frontDefault = frontDefault,
+        _artwork = artwork;
+
+  final String? _frontDefault;
+  final String? _artwork;
+
+  String get frontDefault => _frontDefault ?? "";
+  String get artwork => _artwork ?? "";
+
   factory SpritesDetails.fromJson(Map json) => SpritesDetails(
         frontDefault: json['front_default'],
+        artwork: json['other']['official-artwork']['front_default'],
       );
 }
