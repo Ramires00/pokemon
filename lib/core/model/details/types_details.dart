2c6483c01f501ba836526c41f6d33d107554fdb5
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2023-05-18 19:51:47 -0300

    🏗️ adjusted models

--- lib/core/model/details/types_details.dart
+++ lib/core/model/details/types_details.dart
@@ -1,4 +1,18 @@
 class TypesDetails {
-  TypesDetails();
-  factory TypesDetails.fromJson(Map json) => TypesDetails();
+  TypesDetails({
+    String? name,
+    String? url,
+  })  : _name = name,
+        _url = url;
+
+  final String? _name;
+  final String? _url;
+
+  String get name => _name ?? "";
+  String get url => _url ?? "";
+
+  factory TypesDetails.fromJson(Map json) => TypesDetails(
+        name: json['name'],
+        url: json['url'],
+      );
 }
