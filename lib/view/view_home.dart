54cf5190a62dbd265b3a83cc97c5f870cbff4440
Victor <vctudo.27@gmail.com>
2023-05-20 16:08:40 -0300

    💄 listing pokemon names and images

--- lib/view/view_home.dart
+++ lib/view/view_home.dart
@@ -1,6 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:pokemon/core/data.dart';
+import 'package:pokemon/core/extensions/ext_string.dart';
+import 'package:pokemon/core/model/pokemon_metadata.dart';
 import 'package:pokemon/viewmodel/viewmodel_home.dart';
 
 class ViewHome extends StatelessWidget {
@@ -19,8 +23,14 @@ class ViewHome extends StatelessWidget {
         builderDelegate: _builderDelegate(),
       );
 
-  PagedChildBuilderDelegate _builderDelegate() => PagedChildBuilderDelegate(
+  PagedChildBuilderDelegate<PokemonMetadata> _builderDelegate() =>
+      PagedChildBuilderDelegate<PokemonMetadata>(
         itemBuilder: (context, item, index) => ListTile(
+          trailing: CircleAvatar(
+            child: CachedNetworkImage(
+              imageUrl: baseUrlSprite + item.url.pokemonImage,
+            ),
+          ),
           title: Text(item.name),
         ),
       );
