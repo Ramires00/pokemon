6d451f33dadd9cb16d32d20aaa04cca23dc11abb
Victor <vctudo.27@gmail.com>
2023-05-20 14:20:59 -0300

    🏗️ added usecases

--- lib/viewmodel/viewmodel_home.dart
+++ lib/viewmodel/viewmodel_home.dart
@@ -1,27 +1,42 @@
 import 'package:get/get.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-import 'package:pokemon/core/model/pokemon_data.dart';
-import 'package:pokemon/repo/repository_pokemon_data.dart';
+import 'package:pokemon/core/model/pokemon_metadata.dart';
+import 'package:pokemon/usecase/usecase_get_pokemondata.dart';
 
 class ViewModelHome extends GetxController {
-  ViewModelHome({required this.repo});
+  ViewModelHome({required UsecaseGetPokemonData usecase}) : _usecase = usecase;
 
-  final RepositoryPokemonData repo;
+  final UsecaseGetPokemonData _usecase;
 
-  int pageKey = 0;
+  RxInt pageKey = RxInt(0);
 
   @override
-  void onReady() {
+  void onInit() {
     pagingController.addPageRequestListener((_) async {
       fetchNewPage();
-      pageKey = pageKey + 20;
+      pageKey.value = pageKey.value + 20;
+      refresh();
     });
 
-    super.onReady();
+    super.onInit();
   }
 
-  final PagingController pagingController = PagingController(firstPageKey: 0);
+  final PagingController pagingController = PagingController<int, PokemonMetadata>(
+    firstPageKey: 0,
+  );
 
-  Future<PokemonData> fetchNewPage() async =>
-      await repo.get(pageKey.toString());
+  Future fetchNewPage() async {
+    await _usecase.execute(
+      pageKey.value.toString(),
+    );
+
+    if (_hasNextPage()) {
+      pagingController.appendPage(_usecase.data.value.pokemonMetadatas, pageKey.value);
+    } else {
+      pagingController.appendLastPage(_usecase.data.value.pokemonMetadatas);
+    }
+    refresh();
+  }
+
+  bool _hasNextPage() => _usecase.data.value.next.isNotEmpty;
 }
