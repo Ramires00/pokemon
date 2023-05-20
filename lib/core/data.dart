15c236fb7a79af3780138fee04c0c2aab2d7bfe4
Victor <vctudo.27@gmail.com>
2023-05-20 16:09:18 -0300

    💄 listing pokemon names and images

--- lib/core/data.dart
+++ lib/core/data.dart
@@ -1,2 +1,7 @@
 const String baseUrl = "https://pokeapi.co/";
 const String endpointPokemonData = "api/v2/pokemon";
+
+const String baseUrlSprite =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+const String spriteOtherOfficialArtwork = "other/offical-artwork/";
