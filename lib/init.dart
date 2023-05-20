b32cda417ddec73d6222c6b9754df952a41768ec
Victor <vctudo.27@gmail.com>
2023-05-20 14:19:26 -0300

    🏗️ added usecases

--- lib/init.dart
+++ lib/init.dart
@@ -2,16 +2,16 @@ import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:pokemon/repo/repository_pokemon.dart';
 import 'package:pokemon/repo/repository_pokemon_data.dart';
+import 'package:pokemon/usecase/usecase_get_pokemondata.dart';
 import 'package:pokemon/viewmodel/viewmodel_home.dart';
 
 void _initHttp() => Get.put<Dio>(Dio());
 
 void _initViewModels() {
-  final RepositoryPokemonData _repoPokemonData =
-      Get.find<RepositoryPokemonData>();
+  final UsecaseGetPokemonData usecasePokemonData = Get.find<UsecaseGetPokemonData>();
 
   Get.put<ViewModelHome>(
-    ViewModelHome(repo: _repoPokemonData),
+    ViewModelHome(usecase: usecasePokemonData),
   );
 }
 
@@ -22,8 +22,17 @@ void _initRepositories() {
   Get.put<RepositoryPokemon>(RepositoryPokemon(dio));
 }
 
+void _initUsecases() {
+  final RepositoryPokemonData repoPokemonData = Get.find<RepositoryPokemonData>();
+
+  Get.put<UsecaseGetPokemonData>(
+    UsecaseGetPokemonData(repo: repoPokemonData),
+  );
+}
+
 void init() {
   _initHttp();
   _initRepositories();
+  _initUsecases();
   _initViewModels();
 }
