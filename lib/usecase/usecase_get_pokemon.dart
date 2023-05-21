ee0ca2b6a7d85660e232b7ec2eb394fd781fe24a
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2023-05-20 21:00:13 -0300

    🏗️ created usecase to retrieve single pokemon

---
+++ lib/usecase/usecase_get_pokemon.dart
@@ -0,0 +1,21 @@
+import 'package:pokemon/core/interfaces/usecase.dart';
+import 'package:pokemon/core/model/pokemon.dart';
+import 'package:pokemon/repo/repository_pokemon.dart';
+
+class UsecaseGetPokemonData implements Usecase<Pokemon> {
+  UsecaseGetPokemonData({required this.repo});
+
+  final RepositoryPokemon repo;
+
+  Pokemon data = Pokemon();
+
+  @override
+  Future<Pokemon> execute([dynamic args]) => repo.get(args).then<Pokemon>(
+        onExecuteReturn,
+      );
+
+  Future<Pokemon> onExecuteReturn(Pokemon newValueFetched) async {
+    data = newValueFetched;
+    return newValueFetched;
+  }
+}
