0f1cf8efcbfbf603fb3bd0373654904ee0d04d6a
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2023-05-20 22:39:54 -0300

    💄 changed primary

--- lib/view/style/colors.dart
+++ lib/view/style/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF464033);
+  static const Color primary = Colors.white;
   static const Color onPrimary = Color(0xFF140005);
   static const Color secondary = Color(0xFF7E7C73);
   static const Color onSecondary = Color(0xFFBBC4C2);
