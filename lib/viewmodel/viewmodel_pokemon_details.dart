a542fc85f2cfdecaa30c016ccd37f3923ed94ad2
Victor <vctudo.27@gmail.com>
2023-05-23 00:05:04 -0300

    🎨 improved code structure

--- lib/viewmodel/viewmodel_pokemon_details.dart
+++ lib/viewmodel/viewmodel_pokemon_details.dart
@@ -1,17 +1,22 @@
 import 'package:get/get.dart';
+import 'package:pokemon/core/interfaces/viewmodel.dart';
 import 'package:pokemon/core/model/pokemon.dart';
 import 'package:pokemon/usecase/usecase_get_pokemon.dart';
 
-class ViewModelPokemonDetails extends GetxController {
-  ViewModelPokemonDetails({required UsecaseGetPokemon usecase})
-      : _usecase = usecase;
+class ViewModelPokemonDetails extends GetxController with ViewModel<Pokemon> {
+  ViewModelPokemonDetails({required UsecaseGetPokemon usecase}) : _usecase = usecase;
 
   final UsecaseGetPokemon _usecase;
 
   Pokemon pokemon = Pokemon();
 
-  Future<Pokemon> fetchPokemon(String pokemonId) async =>
-      _usecase.execute(pokemonId).then(
-            (value) => pokemon = value,
-          );
+  RxBool isLoading = RxBool(false);
+
+  Future<Pokemon> fetchPokemon(String pokemonId) async => _usecase
+      .execute(pokemonId)
+      .then(
+        (value) => pokemon = value,
+      )
+      .onError(onError)
+      .whenComplete(() => isLoading.value = false);
 }
